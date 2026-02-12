2c5fd1422e70e406d283f0972c7bdc2423449fd1
Kimdesignui <pt.zenco@gmail.com>
2026-02-12 10:41:52 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import App from './App'; // Đảm bảo import đúng App.tsx cùng cấp
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+// Tìm cái "phễu" để đổ dữ liệu vào từ index.html
+const container = document.getElementById('root');
+
+if (!container) {
+  console.error("Không tìm thấy thẻ <div id='root'> trong file index.html!");
+} else {
+  // Sử dụng createRoot theo chuẩn React 18/19 mới nhất
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
